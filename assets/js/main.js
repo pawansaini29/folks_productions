a2f10be3c215fabbb5be4294b84ab2bc55309fbe
Pawan <sainipawan2929@gmail.com>
2024-07-01 15:21:49 +0530

    dd

--- assets/js/main.js
+++ assets/js/main.js
@@ -254,38 +254,38 @@
 
     /*---------- 05. Scroll To Top ----------*/
     // progressAvtivation
-    // if($('.scroll-top')) {    
-    //     var scrollTopbtn = document.querySelector('.scroll-top');
-    //     var progressPath = document.querySelector('.scroll-top path');
-    //     var pathLength = progressPath.getTotalLength();
-    //     progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-    //     progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
-    //     progressPath.style.strokeDashoffset = pathLength;
-    //     progressPath.getBoundingClientRect();
-    //     progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
-    //     var updateProgress = function () {
-    //         var scroll = $(window).scrollTop();
-    //         var height = $(document).height() - $(window).height();
-    //         var progress = pathLength - (scroll * pathLength / height);
-    //         progressPath.style.strokeDashoffset = progress;
-    //     }
-    //     updateProgress();
-    //     $(window).scroll(updateProgress);	
-    //     var offset = 50;
-    //     var duration = 750;
-    //     jQuery(window).on('scroll', function() {
-    //         if (jQuery(this).scrollTop() > offset) {
-    //             jQuery(scrollTopbtn).addClass('show');
-    //         } else {
-    //             jQuery(scrollTopbtn).removeClass('show');
-    //         }
-    //     });				
-    //     jQuery(scrollTopbtn).on('click', function(event) {
-    //         event.preventDefault();
-    //         jQuery('html, body').animate({scrollTop: 0}, 1);
-    //         return false;
-    //     })
-    // }
+    if($('.scroll-top')) {    
+        var scrollTopbtn = document.querySelector('.scroll-top');
+        var progressPath = document.querySelector('.scroll-top path');
+        var pathLength = progressPath.getTotalLength();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+        var updateProgress = function () {
+            var scroll = $(window).scrollTop();
+            var height = $(document).height() - $(window).height();
+            var progress = pathLength - (scroll * pathLength / height);
+            progressPath.style.strokeDashoffset = progress;
+        }
+        updateProgress();
+        $(window).scroll(updateProgress);	
+        var offset = 50;
+        var duration = 750;
+        jQuery(window).on('scroll', function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery(scrollTopbtn).addClass('show');
+            } else {
+                jQuery(scrollTopbtn).removeClass('show');
+            }
+        });				
+        jQuery(scrollTopbtn).on('click', function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, 1);
+            return false;
+        })
+    }
 
     /*---------- 06. Set Background Image ----------*/
     if ($("[data-bg-src]").length > 0) {
